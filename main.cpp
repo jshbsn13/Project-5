b65a58107b4c4f475a8551b2f4d77965b752cfc6
jshbsn13 <Addison2013!!!>
2021-12-10 19:55:46 -0600

    Implementing main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,46 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
+
+
+    string fileName;
+    string fileLine;
+
+    ifstream file;
+
+
+    do {
+        cout << "Please enter the name of the file to open: " << endl;
+
+        cin >> fileName;
+
+        if (fileName == "")
+            cout << "Error! No file name was given!"<<endl;
+
+    } while (fileName == "");
+
+    cout << fileName << endl;
+    file.open(fileName);
+
+    if(file)
+    {
+        getline(file, fileLine);
+
+        while (file)
+        {
+            cout << fileLine << endl;
+        }
+
+    }else
+    {
+        cout<< "Error opening file!"<<endl;
+    }
+
+
 
 
 
